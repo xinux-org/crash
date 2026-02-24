e6deae1e2a3404cfa9e134fd9516a4683ce3be33
xfeusw <khamrakulovkamron@gmail.com>
2026-02-24 18:58:56 +0500

    fix: IllagalAccessError typo is fixed

--- src/IllegalAccess.java
+++ src/IllegalAccess.java
@@ -1,7 +1,7 @@
 public class IllegalAccess {
   public static void main(String[] args) {
     try {
-      throw new IllegalAccessErrorError("This is a simulated illagal access error.");
+      throw new IllegalAccessError("This is a simulated illagal access error.");
     } catch (InternalError e) {
       e.printStackTrace();
     }
