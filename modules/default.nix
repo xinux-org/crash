cdb4cd75bd592317806607a383ae161c7170ac87
xfeusw <khamrakulovkamron@gmail.com>
2026-03-03 20:00:26 +0500

    refactor: module prefix is edited

--- modules/default.nix
+++ modules/default.nix
@@ -8,7 +8,7 @@ flake: {
   pyFiles = map (x: lib.strings.removeSuffix ".py" x.name) (builtins.filter (x: x.value == "regular" && lib.strings.hasSuffix ".py" x.name) (lib.attrsToList (builtins.readDir ../src/python)));
 
   python = la (map (x: {
-      name = x;
+      name = "py-${x}";
       value = import ./python flake {
         pkg = x;
       };
@@ -18,7 +18,7 @@ flake: {
   javaFiles = map (x: lib.strings.removeSuffix ".java" x.name) (builtins.filter (x: x.value == "regular" && lib.strings.hasSuffix ".java" x.name) (lib.attrsToList (builtins.readDir ../src/java)));
 
   java = la (map (x: {
-      name = lib.strings.toLower x;
+      name = lib.strings.toLower "java-${x}";
       value = import ./java flake {pkg = lib.strings.toLower x;};
     })
     javaFiles);
@@ -26,7 +26,7 @@ flake: {
   cFiles = map (x: lib.strings.removeSuffix ".c" x.name) (builtins.filter (x: x.value == "regular" && lib.strings.hasSuffix ".c" x.name) (lib.attrsToList (builtins.readDir ../src/c)));
 
   c = la (map (x: {
-      name = lib.strings.toLower x;
+      name = lib.strings.toLower "c-${x}";
       value = import ./c flake {pkg = lib.strings.toLower x;};
     })
     cFiles);
